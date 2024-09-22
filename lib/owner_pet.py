3fe813cd715a1ef4a8eacd5bf54babc67006f36b
gideonkkk <gideon.kariuki@student.moringaschool.com>
2024-09-22 23:17:31 +0300

    completed

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
+
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+
+        # self.pet_type = pet_type
+
+        if pet_type.lower() not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type.")
+
+        self.pet_type = pet_type.lower()
+        self.owner = owner
+        self.__class__.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [Pet for Pet in Pet.all if Pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise ValueError ("invalid pet type!")
+        
+    def get_sorted_pets(self):
+        owner_pets = self.pets()
+        sorted_pets = sorted(owner_pets, key=lambda x: x.name)
+        return sorted_pets
